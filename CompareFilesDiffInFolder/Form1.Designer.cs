d53123009fe8a9076cea08d1e2e2631fec0a4cce
sai wanza <q020385791@gmail.com>
2021-03-29 14:24:47 +0800

    加入專案檔案。

---
+++ CompareFilesDiffInFolder/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace CompareFilesDiffInFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSetSourceFolder = new System.Windows.Forms.Button();
+            this.labSource = new System.Windows.Forms.Label();
+            this.labTarget = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(916, 440);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(108, 42);
+            this.btnCompare.TabIndex = 0;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 143);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(898, 339);
+            this.txtLog.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SetTargetFolder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSetSourceFolder
+            // 
+            this.btnSetSourceFolder.Location = new System.Drawing.Point(24, 27);
+            this.btnSetSourceFolder.Name = "btnSetSourceFolder";
+            this.btnSetSourceFolder.Size = new System.Drawing.Size(140, 34);
+            this.btnSetSourceFolder.TabIndex = 4;
+            this.btnSetSourceFolder.Text = "SetSourceFolder";
+            this.btnSetSourceFolder.UseVisualStyleBackColor = true;
+            this.btnSetSourceFolder.Click += new System.EventHandler(this.btnSetSourceFolder_Click);
+            // 
+            // labSource
+            // 
+            this.labSource.AutoSize = true;
+            this.labSource.Location = new System.Drawing.Point(189, 43);
+            this.labSource.Name = "labSource";
+            this.labSource.Size = new System.Drawing.Size(153, 18);
+            this.labSource.TabIndex = 6;
+            this.labSource.Text = "Please Select Source";
+            // 
+            // labTarget
+            // 
+            this.labTarget.AutoSize = true;
+            this.labTarget.Location = new System.Drawing.Point(189, 96);
+            this.labTarget.Name = "labTarget";
+            this.labTarget.Size = new System.Drawing.Size(151, 18);
+            this.labTarget.TabIndex = 7;
+            this.labTarget.Text = "Please Select Target";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 512);
+            this.Controls.Add(this.labTarget);
+            this.Controls.Add(this.labSource);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSetSourceFolder);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnCompare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSetSourceFolder;
+        private System.Windows.Forms.Label labSource;
+        private System.Windows.Forms.Label labTarget;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ CompareFilesDiffInFolder/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompareFilesDiffInFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = "";
+            string SourcePath = labSource.Text;
+            string TargetPath =labTarget.Text;
+            DirectoryInfo dirSource = new DirectoryInfo(SourcePath);
+            DirectoryInfo dirTarget = new DirectoryInfo(TargetPath);
+            //取得目錄底下所有資料夾
+            DirectoryInfo[] SubDir= dirSource.GetDirectories("*",SearchOption.AllDirectories);
+
+            foreach (var Dir in SubDir)
+            {
+                string TargetDir = Dir.FullName.Replace(SourcePath, TargetPath);
+                if (!Directory.Exists(TargetDir))
+                {
+                    txtLog.Text += "目標資料夾不存在：" + TargetDir+Environment.NewLine;
+                }
+
+                IEnumerable<FileInfo> ListSource = Dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+
+                FileCompare MyFileCompare = new FileCompare();
+
+                //取出所有Source檔案路徑
+                foreach (var F in ListSource)
+                {
+                    //根目錄轉換
+                    string OldPath = F.FullName;
+                    string NewFilePath = F.FullName.Replace(SourcePath, TargetPath);
+                    if (!File.Exists(NewFilePath))
+                    {
+                        txtLog.Text += "目標檔案不存在：" + NewFilePath + Environment.NewLine;
+                    }
+                    else
+                    {
+                        //檔案若存在則進行檔案比對
+                        FileCompare FC = new FileCompare();
+                        FileInfo OldFile = new FileInfo(OldPath);
+                        FileInfo NewFile = new FileInfo(NewFilePath);
+                        if (OldFile != null && NewFile != null)
+                        {
+                            if (!FC.CheckInfo(OldFile, NewFile))
+                            {
+                                txtLog.Text += "檔案不同步：" + NewFile.FullName + Environment.NewLine;
+                            }
+                        }
+
+
+                    }
+
+                }
+            }
+       
+
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSetSourceFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                labSource.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                labTarget.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        public class FileCompare : IEqualityComparer<FileInfo>
+        {
+            public bool Equals(FileInfo FileSource, FileInfo FileTarget)
+            {
+                //比對名稱與長度
+                return (FileSource.Name == FileTarget.Name && FileSource.Length == FileTarget.Length);
+            }
+            public int GetHashCode(FileInfo File)
+            {
+                //回傳雜湊碼
+                string s = $"{File.Name}{File.Length}";
+                return s.GetHashCode();
+            }
+
+            //內文比對
+            public bool CheckInfo(FileInfo FileSource, FileInfo FileTarget)
+            {
+                var InfoSource = File.ReadAllLines(FileSource.FullName);
+                var InfoTarget = File.ReadAllLines(FileTarget.FullName);
+                var IFequal = Enumerable.SequenceEqual(InfoSource, InfoTarget);
+                return IFequal;
+            }
+        }
+
+
+        public void CheckSameFiles(IEnumerable<FileInfo> QueryCommonFiles, IEnumerable<FileInfo> ListTarget, FileCompare MyFileCompare) 
+        {
+            foreach (var ItemSource in QueryCommonFiles)
+            {
+                //用來源名稱取得目標名稱
+                var ItemTarget = ListTarget.Select(x => x).Where(y => y.Name == ItemSource.Name).FirstOrDefault();
+
+                //察看檔案是否相同
+                bool IFSameFile = MyFileCompare.CheckInfo(ItemSource, ItemTarget);
+                if (IFSameFile)
+                {
+                    //目前檔案若相同則不動作
+                    txtLog.Text +="檔案相同：" + ItemTarget.FullName + Environment.NewLine;
+                }
+                else
+                {
+                    txtLog.Text += "檔案不同步：" + ItemTarget.FullName + Environment.NewLine;
+                }
+            }
+
+        }
+    }
+}
