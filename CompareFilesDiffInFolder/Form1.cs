7b19fe2d7677754265bae75306ce2b54aa3dd398
sai wanza <q020385791@gmail.com>
2021-03-29 15:21:16 +0800

    修改：若根資料夾底下無子資料夾，則做資料全比對

--- CompareFilesDiffInFolder/Form1.cs
+++ CompareFilesDiffInFolder/Form1.cs
@@ -20,48 +20,114 @@ namespace CompareFilesDiffInFolder
 
         private void btnCompare_Click(object sender, EventArgs e)
         {
-            txtLog.Text = "";
-            string SourcePath = labSource.Text;
-            string TargetPath = labTarget.Text;
-            DirectoryInfo dirSource = new DirectoryInfo(SourcePath);
-            DirectoryInfo dirTarget = new DirectoryInfo(TargetPath);
-            //取得目錄底下所有資料夾
-            DirectoryInfo[] SubDir = dirSource.GetDirectories("*", SearchOption.AllDirectories);
-
-            foreach (var Dir in SubDir)
+            try
             {
-                string TargetDir = Dir.FullName.Replace(SourcePath, TargetPath);
-                if (!Directory.Exists(TargetDir))
+                txtLog.Text = "";
+                string SourcePath = labSource.Text;
+                string TargetPath = labTarget.Text;
+                DirectoryInfo dirSource = new DirectoryInfo(SourcePath);
+                DirectoryInfo dirTarget = new DirectoryInfo(TargetPath);
+                //取得目錄底下所有資料夾
+                DirectoryInfo[] SubDir = dirSource.GetDirectories("*", SearchOption.AllDirectories);
+                //如果該目錄有資料夾
+                if (SubDir.Count() > 0)
                 {
-                    txtLog.Text += "目標資料夾不存在：" + TargetDir + Environment.NewLine;
-                }
-                IEnumerable<FileInfo> ListSource = Dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
-                //取出所有Source檔案路徑
-                foreach (var F in ListSource)
-                {
-                    //根目錄轉換
-                    string OldPath = F.FullName;
-                    string NewFilePath = F.FullName.Replace(SourcePath, TargetPath);
-                    if (!File.Exists(NewFilePath))
+                    foreach (var Dir in SubDir)
                     {
-                        txtLog.Text += "目標檔案不存在：" + NewFilePath + Environment.NewLine;
-                    }
-                    else
-                    {
-                        //檔案若存在則進行檔案比對
-                        FileCompare FC = new FileCompare();
-                        FileInfo OldFile = new FileInfo(OldPath);
-                        FileInfo NewFile = new FileInfo(NewFilePath);
-                        if (OldFile != null && NewFile != null)
+                        txtLog.Text += "----------------開始偵測目標資料夾：" + Dir.FullName + "-----------------------" + Environment.NewLine;
+
+                        string TargetDir = Dir.FullName.Replace(SourcePath, TargetPath);
+                        if (!Directory.Exists(TargetDir))
                         {
-                            if (!FC.CheckInfo(OldFile, NewFile))
+                            txtLog.Text += "目標資料夾不存在：" + TargetDir + Environment.NewLine;
+                        }
+                        IEnumerable<FileInfo> ListSource = Dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+
+
+                        //取出所有Source檔案路徑
+                        foreach (var F in ListSource)
+                        {
+                            //根目錄轉換
+                            string OldPath = F.FullName;
+                            string NewFilePath = F.FullName.Replace(SourcePath, TargetPath);
+                            if (!File.Exists(NewFilePath))
                             {
-                                txtLog.Text += "檔案不同步：" + NewFile.FullName + Environment.NewLine;
+                                txtLog.Text += "目標檔案不存在：" + NewFilePath + Environment.NewLine;
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    //檔案若存在則進行檔案比對
+                                    FileCompare FC = new FileCompare();
+                                    FileInfo OldFile = new FileInfo(OldPath);
+                                    FileInfo NewFile = new FileInfo(NewFilePath);
+                                    if (OldFile != null && NewFile != null)
+                                    {
+                                        if (!FC.CheckInfo(OldFile, NewFile))
+                                        {
+                                            txtLog.Text += "檔案不同步：" + NewFile.FullName + Environment.NewLine;
+                                        }
+
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+
+                                    txtLog.Text += "錯誤：" + ex.InnerException;
+                                }
+
                             }
                         }
+                        txtLog.Text += "----------------偵測目標資料夾結束：" + Dir.FullName + "-----------------------" + Environment.NewLine;
+
                     }
+
+                    txtLog.Text += "比對完畢";
                 }
+                else 
+                {
+                    //如果該資料夾內只有檔案
+                    string[] SourcePaths=Directory.GetFiles(SourcePath);
+                    foreach (string  ThisSourcePath in SourcePaths)
+                    {
+                        string thisTargetPath = ThisSourcePath.Replace(SourcePath,TargetPath);
+                        if (File.Exists(thisTargetPath))
+                        {
+                            //檔案若存在則進行檔案比對
+                            FileCompare FC = new FileCompare();
+                            FileInfo OldFile = new FileInfo(ThisSourcePath);
+                            FileInfo NewFile = new FileInfo(thisTargetPath);
+                            if (OldFile != null && NewFile != null)
+                            {
+                                if (!FC.CheckInfo(OldFile, NewFile))
+                                {
+                                    txtLog.Text += "檔案不同步：" + NewFile.FullName + Environment.NewLine;
+                                }
+
+                            }
+                        }
+                        else 
+                        {
+                            txtLog.Text += "目標檔案不存在：" + thisTargetPath + Environment.NewLine;
+
+                        }
+
+                    }
+
+
+
+
+                }
+
+              
             }
+            catch (Exception ex)
+            {
+
+                txtLog.Text+=ex.InnerException;
+            }
+           
         }
 
 
